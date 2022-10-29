7d3c9e70ab6f4dec768113bf80b12dd0e69de190
motyamiracle@gmail.com <motyamiracle@gmail.com>
2022-10-29 14:20:20 +0300

    Try to succeed

---
+++ src/main/java/StepUser.java
@@ -0,0 +1,12 @@
+import model.UserResponseModel;
+import model.UserRequestModel;
+
+public class StepUser {
+    ApiUser apiUser = new ApiUser();
+
+    public UserResponseModel createUser(String name, String job){
+        UserRequestModel model = new UserRequestModel(name, job);
+        return apiUser.createUser(model);
+
+    }
+}
